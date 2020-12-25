8c47730180b6fa94328ef30b4bcd8227bf9f810d
babanb <baban.b1@gmail.com>
2020-12-26 01:36:25 +0530

    Initial Commit

---
+++ amistee-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  amistee-app
+//
+//  Created by Apple on 26/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ amistee-app/amistee_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  amistee_appApp.swift
+//  amistee-app
+//
+//  Created by Apple on 26/12/20.
+//
+
+import SwiftUI
+
+@main
+struct amistee_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
